da7733d477ee5f83721cd7ae88a7dfcc0770250a
iruhan004-byte <iruhan004@gmail.com>
2026-08-12 12:02:59 +0600

    Update agents.py

--- agents.py
+++ agents.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 load_dotenv()
-llm=ChatGroq(model="llama-3.3-70b-versatile", temperature=0)
+llm=ChatGroq(model="llama-3.1-8b-instant", temperature=0)
 
 def build_research_agent():
     return create_agent(
